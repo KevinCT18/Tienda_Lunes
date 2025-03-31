32687cfd553b8eeede1f96a4016d30f973f4d87c
CASCANTE-TAPIA <CASCANTE-TAPIA@DESKTOP-NO07TTF>
2025-03-31 13:04:53 -0600

    Semana 11

--- src/main/java/com/tienda/repository/UsuarioRepository.java
+++ src/main/java/com/tienda/repository/UsuarioRepository.java
@@ -4,5 +4,16 @@ import com.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+    
+    //Se usa para el proceso de login
     Usuario findByUsername(String username);
+    
+    //Se usa para buscar un registro de usuario en el proceso de activación de un nuevo usuario 
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    //Se utiliza para validar si dentro de la tabla usuario ya existe con el username o el correo indicados
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    //Se utiliza para validar si dentro de la tabla usuario ya existe con el username o el correo indicados
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
